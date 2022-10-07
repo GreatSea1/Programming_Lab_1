aee743364e31e72d531b3052e85ee509260c055d
bjiake <suyd@list.ru>
2022-10-08 00:46:27 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && ( components != null )) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title5 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbRightBorder = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вычислитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(0, 27);
+            this.chart1.Name = "chart1";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.LabelBorderWidth = 3;
+            series5.Name = "Series1";
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.LabelBorderWidth = 3;
+            series6.Name = "Series2";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(785, 322);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title5.Name = "Title1";
+            title5.Text = "Время";
+            title6.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            title6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title6.IsDockedInsideChartArea = false;
+            title6.Name = "Title2";
+            title6.Text = "Деньги";
+            this.chart1.Titles.Add(title5);
+            this.chart1.Titles.Add(title6);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(312, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.Location = new System.Drawing.Point(34, 393);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(272, 31);
+            this.tbInput.TabIndex = 2;
+            // 
+            // tbRightBorder
+            // 
+            this.tbRightBorder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbRightBorder.Location = new System.Drawing.Point(528, 393);
+            this.tbRightBorder.Name = "tbRightBorder";
+            this.tbRightBorder.Size = new System.Drawing.Size(206, 31);
+            this.tbRightBorder.TabIndex = 2;
+            this.tbRightBorder.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbRightBorder_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вычислитьToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // вычислитьToolStripMenuItem
+            // 
+            this.вычислитьToolStripMenuItem.Name = "вычислитьToolStripMenuItem";
+            this.вычислитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.вычислитьToolStripMenuItem.Text = "Вычислить";
+            this.вычислитьToolStripMenuItem.Click += new System.EventHandler(this.вычислитьToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Формула для расчета";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(525, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Крайний срок";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbRightBorder);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Приложение";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbRightBorder;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вычислитьToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.mariuszgromada.math.mxparser;
+using org.mariuszgromada.math.mxparser.mathcollection;
+using org.mariuszgromada.math.mxparser.parsertokens;
+using org.mariuszgromada.math.mxparser.syntaxchecker;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            DrawChart();
+            DrawDichotomy();
+        }
+
+        #region Нахождение дихотомии
+        private double eps = 0.001;
+        private double dichotomyLeftBorder = 0;
+        private double dicphotomyRightBorder;
+        
+        private double Dichotomy() {
+            double dichotomyPoint;
+            dicphotomyRightBorder = Convert.ToDouble(tbRightBorder.Text);
+
+            do {
+                dichotomyPoint = ( dichotomyLeftBorder + dicphotomyRightBorder ) / 2.0;
+
+                if(parseMath(dichotomyLeftBorder) * parseMath(dichotomyPoint) < 0) {
+                    dicphotomyRightBorder = dichotomyPoint;
+                }
+                else if (parseMath(dicphotomyRightBorder) * parseMath(dichotomyPoint) < 0) {
+                    dichotomyLeftBorder = dichotomyPoint;
+                } else {
+                    MessageBox.Show("Точка дихотомии не найдена");
+                    return 0;
+                }
+            } while (Math.Abs(dicphotomyRightBorder - dichotomyLeftBorder) > eps);
+
+            return dichotomyPoint;
+        }
+        #endregion
+
+        #region Отрисовывание дихотомии
+        private void DrawDichotomy() {
+            double dichotomyPointX = Dichotomy();
+            this.chart1.Series[1].Points.Clear();
+            for (int i = -4; i < 5; i++) {
+                this.chart1.Series[1].Points.AddXY(dichotomyPointX, i);
+            }
+        }
+        #endregion
+
+        #region построение графика
+
+        private double leftBorder = 0;
+        private double rightBorder;
+        private double step = 0.1;
+        private double x;
+        private double y = 0;
+
+        private void DrawChart() {
+            rightBorder = Convert.ToDouble(tbRightBorder.Text);//Правая граница
+            x = leftBorder;
+
+            //Очистка графика
+            this.chart1.Series[0].Points.Clear();
+
+            while (x <= rightBorder) {
+                y = parseMath(x);
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += step;
+            }
+        }
+        #endregion
+
+        #region парсинг
+        private double parseMath(double point) {
+            Argument x = new Argument("x");
+            x.setArgumentValue(point);
+            Expression expression = new Expression(tbInput.Text.ToString(), x);
+            return expression.calculate();
+        }
+        #endregion
+
+        private void tbRightBorder_KeyPress(object sender, KeyPressEventArgs e) {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number)) {
+                e.Handled = true;
+            }
+        }
+
+        private void вычислитьToolStripMenuItem_Click(object sender, EventArgs e) {
+            button1_Click(sender, e);
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e) {
+            tbInput.Text = null;
+            tbRightBorder.Text = null;
+        }
+    }
+}
